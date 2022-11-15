d0deffd9146fedc4b50800e00c732db496cc259d
timohuennebeck <hello@timohuennebeck.com>
2022-11-15 16:39:54 +0100

    adjust font sizes

--- src/components/FeatureElement/FeatureElement.jsx
+++ src/components/FeatureElement/FeatureElement.jsx
@@ -4,8 +4,8 @@ export default function FeatureElement({number, element, name}) {
     return (
         <div className="feature">
             <div className="feature__amount">
-                <h1 className="feature__amount-number">{number}</h1>
-                <h1 className="feature__amount-element">{element}</h1>
+                <h2 className="feature__amount-number">{number}</h2>
+                <h2 className="feature__amount-element">{element}</h2>
             </div>
             <h3 className="feature__name">{name}</h3>
         </div>

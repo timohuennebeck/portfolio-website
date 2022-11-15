84b6dead723e11d1d7aad47ddca0f7d552f32532
timohuennebeck <hello@timohuennebeck.com>
2022-11-15 18:48:05 +0100

    add about page desktop design

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,37 +2,49 @@ import "./AboutPage.scss";
 
 import FeatureElement from "../../components/FeatureElement/FeatureElement";
 import ImageFrame from "../../components/ImageFrame/ImageFrame";
-import ButtonElement from "../../components/ButtonElement/ButtonElement";
 import microsoftImg from "../../assets/images/microsoft-certificate.jpg";
+import ButtonElement from "../../components/ButtonElement/ButtonElement";
 import LearnMoreField from "../../components/LearnMoreField/LearnMoreField";
 
 export default function AboutPage() {
     return (
-        <div className="about">
-            <div className="about__content">
-                <h1>About</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
+        <>
+            <div className="about">
+                <div className="about__ctn">
+                    <div className="about__ctn-content">
+                        <h1>About</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat.
+                        </p>
+                    </div>
+                    <div className="about__ctn-featured">
+                        <FeatureElement number="1" element="+" name="Years of Experience" />
+                        <FeatureElement number="7" element="+" name="Successful Projects" />
+                    </div>
+                    <div className="about__ctn-btn">
+                        <div>
+                            <ButtonElement name="DOWNLOAD CV" />
+                        </div>
+                    </div>
+                </div>
+                <div className="about__img">
+                    <ImageFrame img={microsoftImg} />
+                    <div className="about__img-btn">
+                        <ButtonElement name="DOWNLOAD CV" />
+                    </div>
+                </div>
             </div>
-            <div className="about__featured">
-                <FeatureElement number="1" element="+" name="Years of Experience" />
-                <FeatureElement number="7" element="+" name="Successful Projects" />
-            </div>
-            <ImageFrame img={microsoftImg} />
-            <ButtonElement name="DOWNLOAD CV" />
-            <div className="about__learn-more">
-                <LearnMoreField
-                    name="ABOUT ME"
-                    paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-                <LearnMoreField
-                    name="ABOUT ME"
-                    paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-            </div>
-        </div>
+            <LearnMoreField
+                name="ABOUT ME"
+                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            />
+            <LearnMoreField
+                name="ABOUT ME"
+                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            />
+        </>
     );
 }

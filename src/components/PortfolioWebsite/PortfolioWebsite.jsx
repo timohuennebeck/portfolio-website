03990e7b8cd435ca7cadbb4f7416f7881f5f6e70
timohuennebeck <hello@timohuennebeck.com>
2022-11-16 16:30:44 +0100

    remove live demo button from portfolio website

--- src/components/PortfolioWebsite/PortfolioWebsite.jsx
+++ src/components/PortfolioWebsite/PortfolioWebsite.jsx
@@ -4,9 +4,9 @@ import ImageFrame from "../ImageFrame/ImageFrame";
 import img from "../../assets/images/microsoft-certificate.jpg";
 import "./PortfolioWebsite.scss";
 
-import portfolioWebsite1 from "../../assets/images/portfolio-website-1.jpg"
-import portfolioWebsite2 from "../../assets/images/portfolio-website-2.jpg"
-import portfolioWebsite3 from "../../assets/images/portfolio-website-3.jpg"
+import portfolioWebsite1 from "../../assets/images/portfolio-website-1.jpg";
+import portfolioWebsite2 from "../../assets/images/portfolio-website-2.jpg";
+import portfolioWebsite3 from "../../assets/images/portfolio-website-3.jpg";
 
 export default function PortfolioWebsite() {
     return (
@@ -16,9 +16,9 @@ export default function PortfolioWebsite() {
                     <h1 className="portfolio-website__ctn-content-header">Portfolio Website</h1>
                     <p>
                         Personal website with a minimalistic design showcasing the most up-to-date
-                        projects with their corresponding demos. You'll be able to find my current and
-                        past projects here. For a more in-depth description, please download my CV
-                        or visit my LinkedIn.
+                        projects with their corresponding demos. You'll be able to find my current
+                        and past projects here. For a more in-depth description, please download my
+                        CV or visit my LinkedIn.
                     </p>
                 </div>
                 <div className="portfolio-website__ctn-featured">
@@ -26,9 +26,6 @@ export default function PortfolioWebsite() {
                     <FeatureElement name="Hours Invested" number="15" element="+" />
                 </div>
                 <div className="portfolio-website__ctn-buttons">
-                    <div>
-                        <ButtonElement name="LIVE DEMO" backgroundColor="#FFF" fontColor="#000" />
-                    </div>
                     <div>
                         <ButtonElement
                             name="GITHUB"
@@ -49,7 +46,6 @@ export default function PortfolioWebsite() {
                 <ImageFrame img={portfolioWebsite3} />
             </div>
             <div className="portfolio-website__buttons">
-                <ButtonElement name="LIVE DEMO" backgroundColor="#FFF" fontColor="#000" />
                 <div className="portfolio-website__buttons-github">
                     <ButtonElement
                         name="GITHUB"

af0e05bd2f56a69c9c697648e015054023d1eb73
timohuennebeck <hello@timohuennebeck.com>
2022-11-27 20:33:12 +0100

    add logo

--- src/components/HeaderDesktop/HeaderDesktop.jsx
+++ src/components/HeaderDesktop/HeaderDesktop.jsx
@@ -5,11 +5,10 @@ import NavLink from "../NavLink/NavLink";
 export default function HeaderDesktop() {
     return (
         <nav className="header-desktop">
-            <div className="header-desktop__social">{/* <h2>TIMO HUENNEBECK</h2> */}</div>
+            <div className="header-desktop__name">
+                <NavLink name="Timo Huennebeck" href="#home" />
+            </div>
             <ul className="header-desktop__links">
-                <li>
-                    <NavLink name="Home" href="#home" />
-                </li>
                 <li>
                     <NavLink name="Projects" href="#projects" />
                 </li>
